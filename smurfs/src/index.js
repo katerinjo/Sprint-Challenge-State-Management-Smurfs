5c1605a9a4e218d1298e0f7b22417479be19dcbd
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-16 08:45:01 -0700

    index.js boilerplate

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
