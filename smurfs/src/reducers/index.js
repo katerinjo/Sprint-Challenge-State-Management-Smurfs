a20f47f657c0ac6f9947dca778070a78c17bb663
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-16 09:49:39 -0700

    define states & transitions in two styles, I'll pick one later

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,8 +1,48 @@
 const initState = {};
 
 export const commands = {
-  '!!': '!!'
+  ADD_SMURF: 'ADD_SMURF',
+  ADD_FAIL: 'ADD_FAIL',
+  LOAD_VILLAGE: 'LOAD_VILLAGE',
+  LOAD_SUCCESS: 'LOAD_SUCCESS',
+  LOAD_FAIL: 'LOAD_FAIL'
 };
+
+const states = {
+  STATIC: 'STATIC',
+  ADDING: 'ADDING',
+  ADD_ABORT: 'ADD_ABORT',
+  LOADING: 'LOADING',
+  LOAD_ABORT: 'LOAD_ABORT'
+};
+
+export const [
+  ADD_SMURF,
+  ADD_FAIL,
+  LOAD_VILLAGE,
+  LOAD_SUCCESS,
+  LOAD_FAIL
+ ] = [
+  'ADD_SMURF',
+  'ADD_FAIL',
+  'LOAD_VILLAGE',
+  'LOAD_SUCCESS',
+  'LOAD_FAIL'
+ ];
+
+const [
+  STATIC,
+  ADDING,
+  ADD_ABORT,
+  LOADING,
+  LOAD_ABORT
+] = [
+  'STATIC',
+  'ADDING',
+  'ADD_ABORT',
+  'LOADING',
+  'LOAD_ABORT'
+];
 
 export default (state = initState, action) => {
   switch(action.type) {
